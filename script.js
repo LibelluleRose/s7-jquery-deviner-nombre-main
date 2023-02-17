068644792b41f0006d4a0694d83f5d0070d1d93a
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-02-17 16:13:57 -0500

    Un peu de JS et CSS de fait.

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-let aleatoire = Math.ceil(Math.random()*100); //le nombre à deviner
+let NbrCaché = Math.ceil(Math.random() * 100); //le nombre à deviner
+
+let message =
+    [
+        'Ton nombre est trop grand',
+        'Ton nombre est trop petit',
+        'GAME OVER! Tu as utilisé toute tes chances',
+        'BRAVO! Tu as trouver le nombre cachée!!!'
+    ];
+
+for (let i = 0; i < 10; i++) {
+
+
+//Lire le nombre
+    let Nombre = $("#essai").val;
+
+//Comparer le nbr ak le NbrCaché
+    if (Nombre < NbrCaché) {
+
+
+    } else (Nombre > NbrCaché)
+    {
+
+
+
+    else
+        (Nombre === NbrCaché)
+        {
+
+        }
+        ;
+
+
+    }//fin bouble for
+
+
+
