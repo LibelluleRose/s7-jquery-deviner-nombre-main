39ed4882d84abeb79cef8c45bad1de6d1ffc8cb7
LibelluleRose <0729193@cegepoutaouais.qc.ca>
2023-02-20 12:19:44 -0500

    Le gros du javascript est fait. Il faut que je vois comment faire les nbr qui s'enlève et se déplace, ak un tableau?

--- script.js
+++ script.js
@@ -1,37 +1,93 @@
-let NbrCaché = Math.ceil(Math.random() * 100); //le nombre à deviner
+//Nombre caché
+let NbrCache = Math.ceil(Math.random() * 100); //le nombre à deviner
 
 let message =
     [
-        'Ton nombre est trop grand',
-        'Ton nombre est trop petit',
-        'GAME OVER! Tu as utilisé toute tes chances',
-        'BRAVO! Tu as trouver le nombre cachée!!!'
+        'Trop bas!',
+        'Trop haut!',
+        'Nope!',
+        "Ah non :'( meilleure chance la prochaine fois!",
+        'Bravo tu as réussi!'
     ];
 
-for (let i = 0; i < 10; i++) {
+$(document).ready(function () {
 
+    ////////////////////////////////
+    //fonction
+    ///////////////////////////////
 
-//Lire le nombre
-    let Nombre = $("#essai").val;
+//fonction qui supprime les nbr plus petit
+    function EnleverNombrePlusPetit() {
+        for (let i = 0; i < Nombre; i++) {
+            $("#" + i).hide(); //unexpected tokken à revoir
+        }
+    }
 
-//Comparer le nbr ak le NbrCaché
-    if (Nombre < NbrCaché) {
+    //fonction qui supprime les nbr plus grand
+    function EnleverNombrePlusGrand() {
+        for (let i = Nombre; i < 100; i++) {
+            $("#" + i).hide(); //unexpected tokken à revoir
+        }
+    }
 
+    //Fonction qui affiche les messages selon si réussi à trouver nombre
+    function AfficherMessage(reussi) {
+        if (false) {
+            $("#BarreMessage").css("background-color", "red");
+            $("#BarreMessage").text(message[3]);
 
-    } else (Nombre > NbrCaché)
-    {
-
-
-
-    else
-        (Nombre === NbrCaché)
-        {
 
         }
-        ;
+        if (true) {
+            $("#BarreMessage").css("background-color", "green");
+            $("#BarreMessage").text(message[4]);
+            $("#" + NbrCaché).css("background-color", "green");
+        }
+    }
+
+    //////////////////////////////////////////
+    //boucle code
+    ////////////////////////////////////
+
+//boucle pour les 10 essais
+    for (let i = 0; i < 10; i++) {
+
+
+$("#submit").click(function (){
+
+    //Lire le nombre
+    //todo: Vérifier avec le prof sur pk erreur nan
+    let Nombre = 56;
+    //Nombre = parseInt($("#essai").val);
+
+    //Comparer le nbr ak le NbrCaché
+    if (Nombre < NbrCache) {
+        EnleverNombrePlusPetit();
+        AfficherMessage(false);
+        $("#Message").text(message[0]);
+
+    } else if (Nombre > NbrCache) {
+        EnleverNombrePlusGrand();
+        AfficherMessage(false);
+        $("#Message").text(message[1]);
+
+    } else if
+        (Nombre === NbrCache)
+    {
+        AfficherMessage(true);
+    }
+    ;
+
+})
+
+
 
 
     }//fin bouble for
+
+
+})//fin du ready
+
 
 
 
